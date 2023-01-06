6a438df64f304e147b64db9322987fa724ebbfd8
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-01-06 12:55:49 +0000

    TEST: Added Unit tests for LessonDownloadButton

--- resources/ts/test/Components/Lesson/LessonDownloadButton.test.tsx
+++ resources/ts/test/Components/Lesson/LessonDownloadButton.test.tsx
@@ -4,22 +4,46 @@ import { render, screen } from "@testing-library/react";
 // To Test
 import LessonDownloadButton from "@/Components/Lesson/LessonDownloadButton";
 
+const passedClass = "bg-bibletime-pink";
 
-// Test
-test('should render Lesson download button component correctly', () => {
-    //Setup\
-    const passedClass = "bg-bibletime-pink";
-    render(<LessonDownloadButton title={"Jacob"} infoText={null} infoClass={passedClass} getDownloadLink={"abcde"} />);
+// Tests
+describe('Lesson download button render', () => {
+    test('should render Lesson download button WITHOUT infotext correctly', () => {
+        //Setup
+        const downloadButtonRender = render(<LessonDownloadButton title={"Jacob"} infoText={null} infoClass={passedClass} downloadLink={"abcde"} />);
 
-    // Pre Expectations
-    expect(screen.getByRole('button')).not.toBeEmptyDOMElement();
-    expect(screen.getByRole("button").classList.contains(`hover:${passedClass}`)).toBeTruthy();
+        const buttonElement = downloadButtonRender.getByRole('button');
 
+        // Pre Expectations
+        expect(buttonElement).not.toBeEmptyDOMElement();
+        expect(buttonElement.classList.contains(`hover:${passedClass}`)).toBeTruthy();
 
-    // Init
+    });
+    test('should render Lesson download button WITH infotext correctly', () => {
+        //Setup
+        const infoText = "Level 0";
+        const downloadButtonRender = render(<LessonDownloadButton title={"Jacob"} infoText={infoText} infoClass={passedClass} downloadLink={"abcde"} />);
 
-    // Post Expectations
+        const buttonElement = downloadButtonRender.getByRole('button');
 
+        // Pre Expectations
+        expect(buttonElement).not.toBeEmptyDOMElement();
+        expect(buttonElement.classList.contains(`hover:${passedClass}`)).toBeTruthy();
+        expect(downloadButtonRender.getByText(infoText)).not.toBeEmptyDOMElement();
 
+    });
 
-});
+    test('should render disabled button component correctly', () => {
+        const downloadButton = render(<LessonDownloadButton title={"Jacob"} infoText={null} infoClass={passedClass} downloadLink={""} />);
+
+        expect(downloadButton.getByRole('button')).not.toBeEmptyDOMElement();
+        expect(downloadButton.getByRole('button')).toBeDisabled();
+
+    })
+
+    test('should render button with no values passed', () => {
+        const downloadButton = render(<LessonDownloadButton title={"Test title"} infoText={null} infoClass={passedClass} downloadLink={null} />);
+
+        expect(downloadButton.getByRole('button')).not.toBeEmptyDOMElement();
+    })
+})
