9b159e3c345a1ff4a389c7367ba55c050c48a052
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-01-18 09:44:53 +0000

    FIX: Event dropdown cosmetic

--- resources/ts/Components/Navigation/DropdownNavLink.tsx
+++ resources/ts/Components/Navigation/DropdownNavLink.tsx
@@ -4,9 +4,11 @@ export default function DropdownNavLink({ href, active, children }: { href: stri
         <Link
             href={href}
             className={
-                active
-                    ? 'inline-flex items-center whitespace-nowrap py-4 px-8 w-full text-gray-50 bg-blue-500 bg-clip-border text-sm font-medium leading-5 font-bold focus:outline-none focus:border-black focus:bg-slate-200 transition duration-150 ease-in-out'
-                    : 'inline-flex items-center whitespace-nowrap py-4 px-8 w-full border-b border-gray-300 text-sm font-medium leading-5 hover:bg-slate-200 hover:rounded-lg focus:outline-none focus:border-black focus:bg-slate-200 transition duration-150 ease-in-out'
+                `inline-flex items-center whitespace-nowrap bg-clip-padding py-4 px-8 w-full text-sm font-medium leading-5 
+                ${active
+                    ? 'text-gray-50 bg-blue-500 bg-clip-padding font-bold'
+                    : 'hover:bg-slate-200'}
+                    focus:outline-none focus:border-black focus:bg-slate-200 transition duration-150 ease-in-out`
             }
         >
             {children}
