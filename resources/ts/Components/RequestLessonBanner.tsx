87ab7968c5cb193d2f0debc8737f3c478be287d4
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-01-25 18:46:30 +0000

    PAGE: Individual form added frontend

--- resources/ts/Components/RequestLessonBanner.tsx
+++ resources/ts/Components/RequestLessonBanner.tsx
@@ -7,8 +7,8 @@ export default function RequestLessonBanner() {
             <div className="w-full p-6 mx-auto space-y-2 md:max-w-2xl">
                 <p className="text-xl font-bold text-center text-white md:text-3xl">Request a lesson for your School or an Individual</p>
                 <div className="flex justify-around">
-                    <Link type="button" href={route('request.school')}><SecondaryButton>School or Group</SecondaryButton></Link>
-                    <Link type="button" href={route('request.home')}><SecondaryButton>Individual or Family</SecondaryButton></Link>
+                    <Link type="button" href={route('request.group')}><SecondaryButton>School or Group</SecondaryButton></Link>
+                    <Link type="button" href={route('request.individual')}><SecondaryButton>Individual or Family</SecondaryButton></Link>
                 </div>
             </div>
         </div>
--- resources/ts/Pages/Front.tsx
+++ resources/ts/Pages/Front.tsx
@@ -37,7 +37,7 @@ export default function Front({ bibleTimeDownloads }: any): JSX.Element {
             <Head title="Home"></Head>
             <section className="relative flex w-full min-h-screen">
                 <div className="flex py-10 justify-center align-center h-full flex-1 bg-center bg-no-repeat bg-cover bg-[url('/hero.jpg')] bg-slate-300 bg-blend-soft-light">
-                    <div className="grid h-full w-4/5 grid-cols-1 gap-2 text-blue-900 my-5 md:grid-cols-[1fr_1fr_20%] md:grid-rows-2">
+                    <div className="grid h-full w-4/5 grid-cols-1 gap-2 text-blue-900 my-5 md:grid-cols-[1fr_1fr_20%]">
                         <div className="bg-white bg-clip-content md:col-span-2 md:row-span-2">
                             <LandingCards
                                 heading="School Study Materials"
@@ -70,7 +70,7 @@ export default function Front({ bibleTimeDownloads }: any): JSX.Element {
                 </div>
             </section>
             <section className="w-full">
-                <div className="grid grid-rows-2 gap-5 p-5 bg-white h-92 md:grid-rows-none md:grid-cols-2 lg:px-20 lg:mx-20 lg:my-2 drop-shadow-lg">
+                <div className="grid gap-5 p-5 bg-white h-92 md:grid-cols-2 lg:px-20 lg:mx-20 lg:my-2 drop-shadow-lg">
                     <div className="flex flex-col text-center uppercase">
                         <h4 className="text-lg italic text-blue-800">This month's Lesson</h4>
                         <Heading3>{`${seriesNames[currentSeriesNumber].code}${currentMonthNumber + 1} - ${monthNames[currentMonthNumber]}`}</Heading3>
