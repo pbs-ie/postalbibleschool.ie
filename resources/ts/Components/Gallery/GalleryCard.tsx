d8fa40eb68a3f5950ce816b8a788ff49f828048e
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-01-25 11:59:11 +0000

    REFACTOR: optimized image load

--- resources/ts/Components/Gallery/GalleryCard.tsx
+++ resources/ts/Components/Gallery/GalleryCard.tsx
@@ -1,11 +1,16 @@
 
 import PlaceholderImage from "@images/Placeholder.svg";
+import { useState } from "react";
 
 export default function GalleryCard({ imageLink, text, cardNumber = null }: { imageLink: string, text: string, cardNumber?: number | null }) {
+    const [loaded, setLoaded] = useState(false);
     return (
         <div className={`relative ${!!cardNumber && cardNumber === 1 ? "col-span-2 row-span-2 md:row-span-3" : "col-span-1"}`}>
-            <img className="relative h-full aspect-[3/2] object-cover" src={imageLink === "" ? PlaceholderImage : imageLink} alt={text === "" ? "Sample image" : text} />
-            <p className="absolute w-full bottom-0 text-lg text-center uppercase font-bold bg-blue-400/80 text-slate-50 py-2">{text}</p>
+            {!loaded &&
+                <img src={PlaceholderImage} alt="Loading image" className="relative aspect-[3/2] object-cover" />
+            }
+            <img className="relative h-full aspect-[3/2] object-cover" src={imageLink === "" ? PlaceholderImage : imageLink} alt={text === "" ? "Sample image" : text} onLoad={() => setLoaded(true)} />
+            <p className="absolute bottom-0 w-full py-2 text-lg font-bold text-center uppercase bg-blue-400/80 text-slate-50">{text}</p>
         </div>
     )
 }
--- resources/ts/Pages/Events/Camp.tsx
+++ resources/ts/Pages/Events/Camp.tsx
@@ -15,6 +15,7 @@ import CampSinging from "@images/camp/camp-singing-min.jpg";
 import CampGames from "@images/camp/camp-games-min.jpg";
 import CampCraft from "@images/camp/camp-craft-min.jpg";
 import CampAdventure from "@images/camp/camp-adventure-min.jpg";
+import ParagraphContainer from "@/Components/Typography/ParagraphContainer";
 
 export default function Camp() {
     const images: Gallery[] = [
@@ -59,19 +60,19 @@ export default function Camp() {
                 </ExtendScreenWrapper>
                 <CardContainer type="camp" />
                 <Heading2>Camp</Heading2>
-                <div className="w-3/4 mb-20 mx-auto">
+                <ParagraphContainer>
                     <Paragraph>Camp is held each summer in mid-July and generally fills up extremely quickly after the forms go out in mid-May. Camp is held at Ovoca Manor just outside the village of Avoca and near Arklow in Co. Wicklow. Ovoca Manor is an outdoor adventure centre owned by Scripture Union. It offers accommodation and a whole range of activities. Our week at camp will typically involve some time in activities at the centre and several trips off site for other activities.</Paragraph>
                     <Paragraph>During the week campers can, among other things, participate in team sports, water sports, craft, activities, shop and visit the beach. Besides activity there is the opportunity to make new friends, explore new places and eat good food. Each day at camp we meet twice to learn from the Bible and have a speaker who seeks to make the messages relevant to young people, this is what we feel is the most important aspect of camp.</Paragraph>
                     <Paragraph>Camp is open to PBS students who are actively completing and returning lessons, and are over the age of 10 on the 1st January of the year in which camp takes place.</Paragraph>
-                </div>
+                </ParagraphContainer>
                 <Heading2>Gallery</Heading2>
 
                 <GalleryAdvanced></GalleryAdvanced>
 
                 <Heading2>History</Heading2>
-                <div className="w-3/4 mb-20 mx-auto">
+                <ParagraphContainer>
                     <Paragraph>Summer camp is something that is very special to many of the young people who have had the opportunity for a week of “summer adventure”. PBS camp began in 1963 in Co. Cork and has visited a few locations down through the years. Most recently the Camp has been held at Ovoca Manor in Co. Wicklow. Some of the young people who have attended camp in recent years have parents who also attended PBS camps. Much has changed from some of those early camps. We have proper toilets, showers and dining halls. When we are travelling it's one person to a seat, instead of “how many can you get in”, but the central purpose of camp remains the same. That purpose is the teaching of the Bible to young people.</Paragraph>
-                </div>
+                </ParagraphContainer>
             </EventWrapper>
         </WrapperLayout>
     )
