3312b39819ebdbf53c0d949588bf9ec0508575f5
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-01-26 15:46:29 +0000

    FIX: Gallery images

--- resources/ts/Components/Gallery/GalleryBasic.tsx
+++ resources/ts/Components/Gallery/GalleryBasic.tsx
@@ -11,7 +11,7 @@ declare global {
 export default function GalleryBasic({ images }: { images: Gallery[] }) {
 
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-1 justify-center mx-10">
+        <div className="grid justify-center grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-4 md:mx-10">
             {images.map(({ title, imageLink }, idx) => (
                 <GalleryCard key={idx} imageLink={imageLink} text={title}></GalleryCard>
             ))}
