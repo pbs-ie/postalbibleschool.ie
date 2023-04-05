cf0f025b2f2c04bd25bc7639d4015e59be908c15
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-04-05 10:40:37 +0100

    FIX: iTeam mobile navigation link added (#17)

--- resources/ts/Components/Typography/Heading1.tsx
+++ resources/ts/Components/Typography/Heading1.tsx
@@ -1,5 +1,5 @@
 export default function Heading1({ children }: { children: React.ReactNode }): JSX.Element {
     return (
-        <><h1 className="p-6 mt-2 mb-4 text-5xl font-bold leading-snug text-blue-800 uppercase font-title">{children}</h1></>
+        <><h1 className="p-6 mt-2 mb-4 text-4xl font-bold leading-snug text-blue-800 uppercase md:text-5xl font-title">{children}</h1></>
     );
 }
