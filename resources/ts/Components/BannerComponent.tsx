74207f26f3f1c6fc1b95674c89a27dad23ff9058
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-08-29 09:53:15 +0100

    UPDATE: Changed landing page card to Camp reunion (#37)
    And responsive changes to navbar and homepage cards

--- resources/ts/Components/BannerComponent.tsx
+++ resources/ts/Components/BannerComponent.tsx
@@ -5,7 +5,7 @@ import AnchorLink from "@/Components/Navigation/AnchorLink";
 export default function BannerComponent({ setShowBanner }: { setShowBanner: Dispatch<SetStateAction<boolean>> }) {
 
     return (
-        <div role="banner" className="relative flex items-center justify-center w-full h-10 text-white bg-bibletime-green">
+        <div role="banner" className="relative flex items-center justify-center w-full pr-5 text-white lg:pr-0 lg:h-10 bg-bibletime-green">
             <p>You can view older assembly videos in the <a className="underline hover:no-underline focus:no-underline" href="/assembly">School Assembly</a> tab</p>
             <button aria-label="Close announcement banner" onClick={() => setShowBanner(false)} className="absolute top-0 right-0 p-2"><CloseIcon className="w-6 h-6" /></button>
         </div>
