8e03fba0b5f2cb0748556d7d3d67eb7e6ae72252
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2023-12-06 14:42:56 +0000

    STEP: Signups for January 2024 active (#61)

--- resources/ts/Pages/Events/Step/Signup.tsx
+++ resources/ts/Pages/Events/Step/Signup.tsx
@@ -1,9 +1,9 @@
 import ButtonLink from "@/Components/Buttons/ButtonLink";
 import PopoutWindow from "@/Components/Icons/PopoutWindow";
 import BasicTable, { TableData } from "@/Components/Tables/BasicTable";
-import Heading1 from "@/Components/Typography/Heading1";
 import Paragraph from "@/Components/Typography/Paragraph";
 import ParagraphContainer from "@/Components/Typography/ParagraphContainer";
+import StepBanner from "@images/step/step-current-banner.png";
 
 import StepWrapper from "@/Layouts/StepWrapper";
 
@@ -11,36 +11,37 @@ export default function Signup() {
     const tableData: TableData[] = [
         {
             heading: "Topic",
-            content: <><p className="font-normal">Study on the book of Job</p></>
+            content: <><p className="font-normal"><b>1 John</b> - Light, Love and Truth</p></>
         },
         {
             heading: "Speaker",
-            content: "John Hewitt"
+            content: "Andrew McNeill"
         },
         {
             heading: "Dates",
-            content: "17th November - 19th November, 2023"
+            content: "19th January - 21st November, 2024"
         },
         {
             heading: "Cost",
-            content: <p className="text-base">Regular - €65<br />Student - €50</p>
+            content: <p className="text-base">Standard - €65<br />Student - €50</p>
         }
     ]
     return (
         <StepWrapper heading="Registration" title={"Sign Up"}>
-            <div className="flex justify-center mb-8">
+            <div className="flex flex-col items-center justify-center w-full gap-4 mb-8 md:flex-row">
                 <BasicTable tableData={tableData}></BasicTable>
+                <img className="h-64 md:h-72" src={StepBanner} alt="Step January 2024 banner - 1 John" />
             </div>
             <ParagraphContainer>
-                <Paragraph>Join us for the upcoming STEP in November 2023 where we take a deeper look at the book of Job. We would encourage you to read/study chapters in the book of Job beforehand. As always, new faces are welcome!</Paragraph>
+                <Paragraph>Join us for the upcoming STEP in January 2024 where we go through the book of 1 John led by Andrew McNeill. We would encourage you to read/study chapters in the book of 1 John beforehand. As always, new faces are welcome!</Paragraph>
 
-                <Paragraph>This event will take place from the 17th - 19th November 2023. Sign up using the form below. To cover the cost of your stay, the price for the weekend will be €65 for regular attendees and €50 for students. Please fill in the form first before making payment. You can either pay using your card by following the link below or at the venue when you arrive.</Paragraph>
+                <Paragraph>You can sign up using the form below. To cover the cost of your stay, the price for the weekend will be €65 for regular attendees and €50 for students. Please fill in the form first before making payment. You can either pay using your card by clicking the button below or at the venue when you arrive.</Paragraph>
 
                 <ButtonLink href={route('payment.step')}>Make Payment <PopoutWindow className="w-5 h-5 ml-2" /></ButtonLink>
 
             </ParagraphContainer>
             <div className="flex items-stretch justify-center my-10">
-                <iframe className="w-full md:w-3/4 max-w-7xl h-[35rem]" src="https://docs.google.com/forms/d/e/1FAIpQLSdYvzFVJEJ6TKU-NdC8DlnJL21o1fQaMSgBbi5gSmh8Epdygw/viewform">Loading…</iframe>
+                <iframe className="w-full md:w-3/4 max-w-7xl h-[35rem]" src="https://docs.google.com/forms/d/e/1FAIpQLSdIoYzAOtd0yUyIDMBtxLk1UjX3ps9z_ACDE8UZ0mcMfmlxgA/viewform">Loading…</iframe>
             </div>
         </StepWrapper>
     )
