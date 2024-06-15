5fcb6ee0015f33181cbb21bb3d2021491e39ba52
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2024-06-15 12:10:29 +0100

    FIX: Download schedule button for STEP (#107)

--- resources/ts/Pages/Events/Step/Schedule.tsx
+++ resources/ts/Pages/Events/Step/Schedule.tsx
@@ -2,6 +2,9 @@ import Paragraph from "@/Components/Typography/Paragraph";
 import ParagraphContainer from "@/Components/Typography/ParagraphContainer";
 import StepWrapper from "@/Layouts/StepWrapper";
 import route from "ziggy-js";
+import StepScheduleFile from "@images/step/step_tt_june_24.pdf"
+import ButtonAnchor from "@/Elements/Buttons/ButtonAnchor";
+import Download from "@/Elements/Icons/Download";
 
 export default function Schedule({ stepSettings }: { stepSettings: StepSettingsProps }) {
     return (
@@ -10,9 +13,11 @@ export default function Schedule({ stepSettings }: { stepSettings: StepSettingsP
                 <Paragraph>Find the schedule for the current STEP ({stepSettings.dates}) below.</Paragraph>
 
             </ParagraphContainer>
+            <ButtonAnchor Icon={Download} href={StepScheduleFile}>Download Schedule</ButtonAnchor>
             {stepSettings.scheduleFileLink &&
                 <div className="flex items-stretch justify-center my-10">
-                    <iframe className="w-full md:w-3/4 max-w-7xl h-[35rem]" src={route('assets.show', stepSettings.scheduleFileLink)}>Loading…</iframe>
+                    {/* <iframe className="w-full md:w-3/4 max-w-7xl h-[35rem]" src={route('assets.show', stepSettings.scheduleFileLink)}>Loading…</iframe> */}
+                    <object className="w-full md:w-3/4 max-w-7xl h-[35rem]" data={route('assets.show', stepSettings.scheduleFileLink)}>Loading…</object>
                 </div>
             }
         </StepWrapper>
