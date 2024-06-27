cdce3960c00b9c11c97ca554ddba5b355aa0c371
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-06-27 16:59:21 +0100

    FEAT: Classroom order changed email updated

--- resources/views/emails/classroom-order-changed.blade.php
+++ resources/views/emails/classroom-order-changed.blade.php
@@ -2,20 +2,11 @@
 
 # Order changes made for {{ $schoolName }}
 
-## New Order values
+View updates made to the order
 
-<x-mail::table>
-| Title | Old value | New value |
-| :---- | --------: | --------: |
-| Classroom Name | {{ $oldClassroom->name }} |{{ $newClassroom->name }} |
-| Curriculum | {{ $oldClassroom->curriculum->name }} |{{ $newClassroom->curriculum->name }} |
-| Level 0 | {{ $oldClassroom->level_0_order }} |{{ $newClassroom->level_0_order }} |
-| Level 1 | {{ $oldClassroom->level_1_order }} |{{ $newClassroom->level_1_order }} |
-| Level 2 | {{ $oldClassroom->level_2_order }} |{{ $newClassroom->level_2_order }} |
-| Level 3 | {{ $oldClassroom->level_3_order }} |{{ $newClassroom->level_3_order }} |
-| Level 4 | {{ $oldClassroom->level_4_order }} |{{ $newClassroom->level_4_order }} |
-| TLP | {{ $oldClassroom->tlp_order }} | {{ $newClassroom->tlp_order }}|
-</x-mail::table>
+<x-mail::button :url="$url">
+View Order
+</x-mail::button>
 
 Thanks,<br>
 {{ config('app.name') }}
