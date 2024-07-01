1fbafba9123e19741b6682b95cc965910c73253b
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-07-01 17:06:03 +0100

    FEAT: 403 page updated

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::layout-custom')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('title', __('Unauthorized'))
+@section('code', '403')
+@section('message', __('You do not have permission to view this page. Please contact info@postalbibleschool.ie if this is a mistake.'))
