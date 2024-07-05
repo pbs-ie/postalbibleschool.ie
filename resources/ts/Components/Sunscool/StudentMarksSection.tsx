e10492a7696d8c10324f103037cd4988daf809ad
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-07-05 14:13:55 +0100

    FEAT: Sunscool Averaging student marks per lesson month

--- resources/ts/Components/Sunscool/StudentMarksSection.tsx
+++ resources/ts/Components/Sunscool/StudentMarksSection.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { createColumnHelper } from "@tanstack/react-table";
 
 export default function StudentMarksSection({ students }: { students: SunscoolStudentProps[] }) {
+
     const flattenedStudentMarks = students.flatMap((student) => {
         return student.lessons.filter((lesson) => !!lesson.bibletime).sort((a, b) => (a.bibletime < b.bibletime ? -1 : 1)).map((lesson) => {
             return {
@@ -13,22 +14,34 @@ export default function StudentMarksSection({ students }: { students: SunscoolSt
         })
     });
 
-    const tableData = useMemo(() => flattenedStudentMarks, [flattenedStudentMarks]);
+    const result = flattenedStudentMarks.reduce((acc, curr) => {
+        if (!acc[curr.name]) {
+            let lessonCodeRegex = /([a-zA-z]\d+)-/.exec(curr.bibletime);
+            let currentBibletime = lessonCodeRegex !== null ? lessonCodeRegex[1] : "";
+            acc[curr.name] = { name: curr.name, totalProgress: 0, count: 0, bibletime: currentBibletime };
+        }
+        acc[curr.name].totalProgress += curr.progress;
+        acc[curr.name].count += 1;
+        return acc;
+    }, {} as Record<string, { name: string, totalProgress: number, count: number, bibletime: string }>);
 
-    const columnHelper = createColumnHelper<typeof flattenedStudentMarks[0]>();
+    const averagedProgress = Object.values(result).map(item => ({
+        name: item.name,
+        averageProgress: item.totalProgress / item.count,
+        bibletime: item.bibletime
+    }));
+
+    const tableData = useMemo(() => averagedProgress, [averagedProgress]);
+
+    const columnHelper = createColumnHelper<typeof averagedProgress[0]>();
 
     const defaultColumns = [
         columnHelper.accessor(row => row.name + "", {
             header: "Student",
         }), columnHelper.accessor(row => row.bibletime ?? "" + "", {
             header: "BibleTime"
-        }), columnHelper.accessor(row => row.title + "", {
-            header: "Title"
-        }), columnHelper.accessor(row => row.progress + "", {
-            header: "Progress",
-            enableColumnFilter: false
-        }), columnHelper.accessor(row => row.done + "", {
-            header: "Done",
+        }), columnHelper.accessor(row => Math.round(row.averageProgress * 100) / 100 + "", {
+            header: "Progress (%)",
             enableColumnFilter: false
         }),
     ]
