5fc950f47862d5251ebfdfcc4b8d2e20680aebdf
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-07-05 17:05:21 +0100

    REFACT: Dashboard Sidebar now using SidebarComponent

--- resources/ts/Components/Navigation/DashboardSidebar.tsx
+++ resources/ts/Components/Navigation/DashboardSidebar.tsx
@@ -1,26 +1,25 @@
-import Play from "@/Elements/Icons/PlayIcon";
+import PlayIcon from "@/Elements/Icons/PlayIcon";
 import VideoCamera from "@/Elements/Icons/VideoCamera";
-import ResourceCard from "@/Components/Cards/ResourceCard";
-import Heading2Alt from "@/Components/Typography/Heading2Alt";
-import List from "@/Elements/Lists/List";
-import ListItem from "@/Elements/Lists/ListItem";
 import route from "ziggy-js";
+import SidebarComponent, { SidebarProps } from "./SidebarComponent";
+import Cog6Tooth from "@/Elements/Icons/Cog6Tooth";
 
 export default function DashboardSidebar() {
+    const sidebarListItems: SidebarProps["listItems"] = [
+        {
+            routeString: route('assembly.index'),
+            isActive: route().current('assembly.index'),
+            Icon: PlayIcon,
+            name: "Assembly Videos"
+        },
+        {
+            routeString: route('assembly.bonus.index'),
+            isActive: route().current('assembly.bonus.index'),
+            Icon: VideoCamera,
+            name: "Bonus Videos"
+        },
+    ];
     return (
-        <div className="flex flex-col items-center w-full p-10 mx-auto bg-sky-100 lg:w-72">
-            <span className="text-center">
-                <Heading2Alt>Additional Resources</Heading2Alt>
-            </span>
-            {/* <div className="flex flex-wrap gap-2 mx-auto lg:flex-col"> */}
-            <List>
-                <ListItem>
-                    <ResourceCard size="small" Icon={Play} href={route('assembly.index')} title="Assembly Videos" />
-                </ListItem>
-                <ListItem>
-                    <ResourceCard size="small" Icon={VideoCamera} href={route('assembly.bonus.index')} title="Bonus Videos" />
-                </ListItem>
-            </List>
-        </div>
+        <SidebarComponent title="Resources" Icon={Cog6Tooth} listItems={sidebarListItems} />
     )
 }
