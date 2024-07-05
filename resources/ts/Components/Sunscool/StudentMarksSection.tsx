378e21f80238b77a67d13d494e98ea1559b2c3ff
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-07-05 14:28:53 +0100

    FIX: Averaging for each month fixed

--- resources/ts/Components/Sunscool/StudentMarksSection.tsx
+++ resources/ts/Components/Sunscool/StudentMarksSection.tsx
@@ -15,13 +15,13 @@ export default function StudentMarksSection({ students }: { students: SunscoolSt
     });
 
     const result = flattenedStudentMarks.reduce((acc, curr) => {
-        if (!acc[curr.name]) {
-            let lessonCodeRegex = /([a-zA-z]\d+)-/.exec(curr.bibletime);
-            let currentBibletime = lessonCodeRegex !== null ? lessonCodeRegex[1] : "";
-            acc[curr.name] = { name: curr.name, totalProgress: 0, count: 0, bibletime: currentBibletime };
+        let lessonCodeRegex = /([a-zA-z]\d+)-/.exec(curr.bibletime);
+        let currentBibletime = lessonCodeRegex !== null ? lessonCodeRegex[1] : "";
+        if (!acc[curr.name + currentBibletime]) {
+            acc[curr.name + currentBibletime] = { name: curr.name, totalProgress: 0, count: 0, bibletime: currentBibletime };
         }
-        acc[curr.name].totalProgress += curr.progress;
-        acc[curr.name].count += 1;
+        acc[curr.name + currentBibletime].totalProgress += curr.progress;
+        acc[curr.name + currentBibletime].count += 1;
         return acc;
     }, {} as Record<string, { name: string, totalProgress: number, count: number, bibletime: string }>);
 
