5bbeaf55cdb83ad2cb94c092f78d39be057cb8de
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-07-05 14:26:31 +0100

    FEAT: Sorting schools lists by name

--- app/Http/Controllers/SunscoolApiController.php
+++ app/Http/Controllers/SunscoolApiController.php
@@ -10,6 +10,9 @@ class SunscoolApiController extends Controller
     public function index()
     {
         $schoolsList = (new SunscoolApiService())->getSchoolsList();
+        usort($schoolsList, fn($a, $b) => (
+            strcasecmp($a->name, $b->name)
+        ));
         return Inertia::render('Settings/Sunscool/Index', [
             'schools' => $schoolsList
         ]);
@@ -18,6 +21,9 @@ class SunscoolApiController extends Controller
     public function classes($schoolId)
     {
         $school = (new SunscoolApiService())->getSchoolDetail($schoolId);
+        usort($school->classes, fn($a, $b) => (
+            strcasecmp($a->name, $b->name)
+        ));
         return Inertia::render('Settings/Sunscool/Classes', [
             'school' => $school
         ]);
