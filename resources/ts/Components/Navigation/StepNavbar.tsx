08a6db2e2ab29c66703e2ea3588e74bc41764d4d
Nischal <10276779+unknwnlistener@users.noreply.github.com>
2024-09-26 14:12:19 +0100

    FEAT: STEP: Schedule file can be removed (#124)
    * MAINTAIN: Node packages updated
    
    * FEAT: STEP: Schedule file can be removed
    
    * FIX: Preserve scroll on delete

--- resources/ts/Components/Navigation/StepNavbar.tsx
+++ resources/ts/Components/Navigation/StepNavbar.tsx
@@ -26,9 +26,11 @@ export default function StepNavbar() {
                         <li className="flex -my-px">
                             <NavLink isSecondary={true} active={route().current('events.step.signup')} href={route('events.step.signup')}>Sign Up</NavLink>
                         </li>
-                        <li className="flex -my-px">
-                            <NavLink isSecondary={true} active={route().current('events.step.schedule')} href={route('events.step.schedule')}>Schedule</NavLink>
-                        </li>
+                        {stepSettings?.scheduleFileLink &&
+                            <li className="flex -my-px">
+                                <NavLink isSecondary={true} active={route().current('events.step.schedule')} href={route('events.step.schedule')}>Schedule</NavLink>
+                            </li>
+                        }
                         <li className="flex -my-px">
                             <NavLink isSecondary={true} active={route().current('payment.step')} href={route('payment.step')}>Payment</NavLink>
                         </li>
