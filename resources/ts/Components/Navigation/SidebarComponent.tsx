0fed68a3ca01988dd76d6c7cb2f161b7d7c7e52e
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-10-09 14:32:31 +0100

    COSM: Sidebar made sticky

--- resources/ts/Components/Navigation/SidebarComponent.tsx
+++ resources/ts/Components/Navigation/SidebarComponent.tsx
@@ -14,14 +14,14 @@ export interface SidebarProps {
 }
 export default function SidebarComponent({ Icon, title, listItems }: SidebarProps) {
     return (
-        <div className="px-4 text-white lg:min-h-[75dvh] lg:max-w-[20dvw] lg:min-w-64 bg-pbsblue">
+        <div className="px-4 z-10 text-white lg:h-dvh sticky top-0 lg:max-w-[20dvw] lg:min-w-64 bg-pbsblue">
             <div className="flex items-center">
                 <span className="p-1 rounded-full bg-pbsblue/20">
                     <Icon className="w-8 h-8 text-white" />
                 </span>
                 <h1 className="p-4 py-6 text-3xl font-bold leading-relaxed text-center text-white uppercase">{title}</h1>
             </div>
-            <nav className="w-full overflow-auto text-white">
+            <nav className="flex w-full gap-4 text-white lg:flex-col">
                 <List>
                     {listItems.map(({ routeString, name, isActive, Icon }) => (
                         <SidebarLink key={routeString} href={routeString} isActive={isActive}>
--- resources/ts/Layouts/WrapperLayout.tsx
+++ resources/ts/Layouts/WrapperLayout.tsx
@@ -48,7 +48,7 @@ export default function WrapperLayout({ showStepNav = false, showCampNav = false
                 <FlashMessage />
                 <div id="modal"></div>
                 <main id="mainContent" className="relative flex flex-col h-full grow md:justify-center">
-                    <div className="w-full overflow-hidden grow">
+                    <div className="w-full grow">
                         {children}
                     </div>
                 </main>
