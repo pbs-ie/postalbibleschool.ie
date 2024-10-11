ef03ed5724c53f33d6d41e7e8cc446bd9c4f495c
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-10-11 12:31:33 +0100

    FEAT: Showing sidebar in Assembly videos section

--- resources/ts/Pages/Assembly/Index.tsx
+++ resources/ts/Pages/Assembly/Index.tsx
@@ -7,6 +7,8 @@ import { getLastElementsOfArray, sortArrayById } from "@/helper";
 import ButtonLink from "@/Elements/Buttons/ButtonLink";
 import route from "ziggy-js";
 import AssemblyGallery from "@/Components/Gallery/AssemblyGallery";
+import SidebarLayout from "@/Layouts/SidebarLayout";
+import DashboardSidebar from "@/Components/Navigation/DashboardSidebar";
 
 export interface AssemblyVideoProps {
     id: number,
@@ -24,50 +26,45 @@ export default function Index({ videoList = [], canViewGallery = false, canEdit 
 
     return (
         <WrapperLayout>
-            <Head title="School Assembly" />
-            <section className="flex flex-col items-center px-10 md:mb-10">
-                <div className="mb-5 md:max-w-4xl">
-                    <Heading1Alt>School Assembly Videos</Heading1Alt>
-                    {canEdit &&
-                        <div className="flex justify-end w-full mb-5">
-                            <div className="inline-flex gap-2 rounded-md">
-                                <ButtonLink href={route('assembly.admin')}>Admin Panel</ButtonLink>
-                                <ButtonLink href={route('assembly.create')}>Add New Video</ButtonLink>
+            <SidebarLayout>
+                <DashboardSidebar canManageCurriculum={canEdit} />
+                <Head title="School Assembly" />
+                <section className="flex flex-col items-center px-10 md:mb-10">
+                    <div className="mb-5 md:max-w-4xl">
+                        <Heading1Alt>School Assembly Videos</Heading1Alt>
+                        {canEdit &&
+                            <div className="flex justify-end w-full mb-5">
+                                <div className="inline-flex gap-2 rounded-md">
+                                    <ButtonLink href={route('assembly.admin')}>Admin Panel</ButtonLink>
+                                    <ButtonLink href={route('assembly.create')}>Add New Video</ButtonLink>
+                                </div>
                             </div>
-                        </div>
-                    }
-                    {canViewGallery &&
-                        <div className="flex justify-end w-full mb-5">
-                            <div className="inline-flex gap-2 rounded-md">
-                                <ButtonLink dataTest="link_bonus_gallery" href={route('assembly.bonus.index')}>View Bonus Videos</ButtonLink>
-                            </div>
-                        </div>
-                    }
-                    <Paragraph>
-                        You will find additional video content for the year 2023/2024 down below. These videos will parallel the BibleTime lessons students are doing for each month of the coming school year. We hope you will find these additional videos helpful.
-                    </Paragraph>
+                        }
+                        <Paragraph>
+                            You will find additional video content for the year 2023/2024 down below. These videos will parallel the BibleTime lessons students are doing for each month of the coming school year. We hope you will find these additional videos helpful.
+                        </Paragraph>
 
-                    {videoList.length > 0 ?
-                        <>
-                            <ul className="grid justify-center grid-cols-1 gap-2 md:grid-cols-2">
-                                {getLastElementsOfArray(sortArrayById(videoList), 2).map((value: AssemblyVideoProps, index) => (
-                                    <li key={index}>
-                                        <VideoHeroCard buttonLink={route('assembly.show', +value.id)} title={(value.title && value.title !== "") ? value.title : value.month} series={value.series} imageLink={value.imageLink} idx={value.id}></VideoHeroCard>
-                                    </li>
-                                ))}
-                            </ul>
+                        {videoList.length > 0 ?
+                            <>
+                                <ul className="grid justify-center grid-cols-1 gap-2 md:grid-cols-2">
+                                    {getLastElementsOfArray(sortArrayById(videoList), 2).map((value: AssemblyVideoProps, index) => (
+                                        <li key={index}>
+                                            <VideoHeroCard buttonLink={route('assembly.show', +value.id)} title={(value.title && value.title !== "") ? value.title : value.month} series={value.series} imageLink={value.imageLink} idx={value.id}></VideoHeroCard>
+                                        </li>
+                                    ))}
+                                </ul>
 
-                            {auth && auth.user && canViewGallery &&
-                                <AssemblyGallery headingText="Previous Assembly Videos" videoList={sortArrayById(videoList)}></AssemblyGallery>
-                            }
-                        </>
-                        :
-                        <div className="italic text-gray-500">No videos added yet.</div>
-                    }
-                </div>
+                                {auth && auth.user && canViewGallery &&
+                                    <AssemblyGallery headingText="Previous Assembly Videos" videoList={sortArrayById(videoList)}></AssemblyGallery>
+                                }
+                            </>
+                            :
+                            <div className="italic text-gray-500">No videos added yet.</div>
+                        }
+                    </div>
 
-            </section>
-
+                </section>
+            </SidebarLayout>
         </WrapperLayout>
     )
 }
