30a524cf58e7aadf32514a31d411e39734eac1f5
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-11-12 11:37:12 +0000

    style: :art: Course level selector css improvement

--- resources/ts/Components/Cards/LessonCard.tsx
+++ resources/ts/Components/Cards/LessonCard.tsx
@@ -15,15 +15,15 @@ interface LessonCard {
 export default function LessonCard({ heading, image, description, type = "bibletime", selectedLevel, setSelectedLevel, isWideScreen, tagCode }: LessonCard) {
     const currentLevel = groupThemes[type].filter(tag => tag.tagCode.toLowerCase() === tagCode.toLowerCase())[0];
     return (
-        <div className={"relative flex flex-col items-center w-auto h-full p-2 py-6 rounded-md md:mb-4 md:mx-2 drop-shadow-lg " + (selectedLevel.tagCode === tagCode ? "md:bg-stone-200" : "bg-stone-100")}>
+        <button onClick={() => setSelectedLevel(currentLevel)} className={"relative flex flex-col items-center w-auto h-full p-2 py-6 rounded-md md:mb-4 md:mx-2 " + (selectedLevel.tagCode === tagCode ? "md:ring-2 md:ring-pbsblue" : "bg-stone-100")}>
             {isWideScreen &&
                 <>
                     <img className="object-fill mx-10 border border-black w-44" src={image} alt={`${heading} image`} />
                     <div className="p-4 pb-8 leading-loose text-center text-gray-700 text-md">{description}</div>
                 </>
             }
-            <button onClick={() => setSelectedLevel(currentLevel)} className="absolute bottom-0 w-full text-center">
-                <TagGroupButton addClass={`font-bold text-base ${currentLevel.tagClass}`}>
+            <div className="absolute bottom-0 w-full text-center">
+                <TagGroupButton addClass={`font-bold text-base transition-[border] ${selectedLevel.tagCode === tagCode ? "border-white/20 border-y-4 " + currentLevel.tagClass : currentLevel.tagClass}`}>
                     <div className="flex flex-row">
                         <p className="grow">{currentLevel.tagName}</p>
                         <svg className="w-6 h-6 ml-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor">
@@ -32,8 +32,8 @@ export default function LessonCard({ heading, image, description, type = "biblet
 
                 </TagGroupButton>
 
-            </button>
-        </div>
+            </div>
+        </button>
     );
 
 }
