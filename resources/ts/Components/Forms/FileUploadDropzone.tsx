9804df78f31b1edb1bdcacc9608bc5da22e30ac2
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-12-23 12:11:22 +0000

    fix: Dropzone made keyboard accessible and has required label

--- resources/ts/Components/Forms/FileUploadDropzone.tsx
+++ resources/ts/Components/Forms/FileUploadDropzone.tsx
@@ -8,20 +8,21 @@ interface FileUploadProps {
     onDrop: React.DragEventHandler,
     accept?: string;
     labelText: string;
+    required?: boolean;
 }
 
-export default function FileUploadDropzone({ name, accept, labelText, onChange, onDrop }: FileUploadProps) {
+export default function FileUploadDropzone({ name, accept, labelText, onChange, onDrop, required = false }: FileUploadProps) {
     return (
-        <div className="inline-flex flex-col gap-2">
-            <InputLabel2>{labelText}</InputLabel2>
-            <label className="p-10 border-2 border-blue-400 border-dashed rounded-lg cursor-pointer hover:bg-gray-100" onDrop={onDrop} onDragOver={(event) => event.preventDefault()} htmlFor={name}>
+        <div className="inline-flex flex-col items-start gap-2">
+            <InputLabel2 htmlFor={name} required>{labelText}</InputLabel2>
+            <label className="p-10 border-2 border-blue-400 border-dashed rounded-lg cursor-pointer hover:bg-gray-100" onDrop={onDrop} onDragOver={(event) => event.preventDefault()} htmlFor={name} aria-required={required}>
                 <div className="flex flex-col items-center gap-2">
                     <CloudArrowUp className="w-10 h-10 text-blue-500" />
-                    <p className="text-gray-800 "><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p className="text-gray-800 "><span className="font-semibold"><button type="button" onClick={() => document.getElementById(name)?.click()}>Click</button> to upload</span> or drag and drop</p>
                     <p className="text-sm text-gray-500">Maximum file size 2 MB</p>
                 </div>
             </label>
-            <FileInput className="hidden" name={name} id={name} onChange={onChange} accept={accept} />
+            <FileInput className="invisible" name={name} id={name} onChange={onChange} accept={accept} required={required} />
         </div>
     )
 }
