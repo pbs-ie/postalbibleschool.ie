c855e6266b2db49310b55324c9fd4f170ea59bc0
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2024-12-31 12:58:56 +0000

    fix: Error handling extended to full html parser

--- app/Services/WorksheetLinksService.php
+++ app/Services/WorksheetLinksService.php
@@ -15,63 +15,64 @@ class WorksheetLinksService
     {
         try {
             $response = Http::get($baseUrl);
+
+            $htmlBody = $response->body();
+
+
+            $htmlParsed = HtmlDomParser::str_get_html($htmlBody);
+            $response = [];
+            $iter = 0;
+
+            foreach ($htmlParsed->find('table tr') as $row) {
+                if (is_null($row)) {
+                    continue;
+                }
+                $linkValue = $row->find('td a', 0);
+                if (is_null($linkValue)) {
+                    continue;
+                }
+
+                $linkHref = $linkValue->href;
+
+                $propertyFiltered = array_filter(
+                    $properties,
+                    fn($property) =>
+                    strpos(strtolower($linkHref), strtolower($property)) !== FALSE
+                );
+                $propertyFiltered = array_values($propertyFiltered);
+
+                if (empty($propertyFiltered)) {
+                    $propertyValue = "undefined";
+                } else {
+                    $propertyValue = $propertyFiltered[0];
+                }
+                if (!isset($response[$propertyValue])) {
+                    $response[$propertyValue] = [];
+                }
+
+                $dateValue = $row->find('td', 2);
+                $sizeValue = $row->find('td', 3);
+                preg_match('/(?:_|-)(A|B|C|D|E)(\d{1,2}).pdf/i', $linkHref, $matchCode, PREG_UNMATCHED_AS_NULL);
+
+                $response[$propertyValue][$iter]["link"] = $baseUrl . trim($linkHref);
+                $response[$propertyValue][$iter]["dateModified"] = is_null($dateValue) ? null : trim($dateValue->innertext);
+                $response[$propertyValue][$iter]["size"] = is_null($sizeValue) ? null : trim($sizeValue->innertext);
+                $response[$propertyValue][$iter]["series"] = is_null($matchCode) || count($matchCode) < 3 ? null : trim(strtoupper($matchCode[1]));
+                $response[$propertyValue][$iter]["monthNumber"] = is_null($matchCode) || count($matchCode) < 3 ? null : (int) trim($matchCode[2]);
+                $iter++;
+            }
+            unset($iter, $linkValue, $dateValue, $sizeValue);
+            unset($response['undefined']);
+
+            // Re-indexing array to 0 index after unsetting the key "undefined"
+            // This is done to receive an array on the view rather than an object with keys of incremnental numbers determined by $iter
+            $response = array_map(fn($propertyGroup) => array_values($propertyGroup), $response);
+
+            return $response;
         } catch (\Exception $e) {
             Log::warning($e);
             return [];
         }
-        $htmlBody = $response->body();
-
-
-        $htmlParsed = HtmlDomParser::str_get_html($htmlBody);
-        $response = [];
-        $iter = 0;
-
-        foreach ($htmlParsed->find('table tr') as $row) {
-            if (is_null($row)) {
-                continue;
-            }
-            $linkValue = $row->find('td a', 0);
-            if (is_null($linkValue)) {
-                continue;
-            }
-
-            $linkHref = $linkValue->href;
-
-            $propertyFiltered = array_filter(
-                $properties,
-                fn($property) =>
-                strpos(strtolower($linkHref), strtolower($property)) !== FALSE
-            );
-            $propertyFiltered = array_values($propertyFiltered);
-
-            if (empty($propertyFiltered)) {
-                $propertyValue = "undefined";
-            } else {
-                $propertyValue = $propertyFiltered[0];
-            }
-            if (!isset($response[$propertyValue])) {
-                $response[$propertyValue] = [];
-            }
-
-            $dateValue = $row->find('td', 2);
-            $sizeValue = $row->find('td', 3);
-            preg_match('/(?:_|-)(A|B|C|D|E)(\d{1,2}).pdf/i', $linkHref, $matchCode, PREG_UNMATCHED_AS_NULL);
-
-            $response[$propertyValue][$iter]["link"] = $baseUrl . trim($linkHref);
-            $response[$propertyValue][$iter]["dateModified"] = is_null($dateValue) ? null : trim($dateValue->innertext);
-            $response[$propertyValue][$iter]["size"] = is_null($sizeValue) ? null : trim($sizeValue->innertext);
-            $response[$propertyValue][$iter]["series"] = is_null($matchCode) || count($matchCode) < 3 ? null : trim(strtoupper($matchCode[1]));
-            $response[$propertyValue][$iter]["monthNumber"] = is_null($matchCode) || count($matchCode) < 3 ? null : (int) trim($matchCode[2]);
-            $iter++;
-        }
-        unset($iter, $linkValue, $dateValue, $sizeValue);
-        unset($response['undefined']);
-
-        // Re-indexing array to 0 index after unsetting the key "undefined"
-        // This is done to receive an array on the view rather than an object with keys of incremnental numbers determined by $iter
-        $response = array_map(fn($propertyGroup) => array_values($propertyGroup), $response);
-
-        return $response;
     }
 
     private static function sendAdminEmail($typeName)
